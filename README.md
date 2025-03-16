e001c93c109b05ef85013e890fe1c7c2428c4b69
Victor Bajada <bajada.victor@gmail.com>
2025-03-15 23:24:56 -0400

    Added README

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
-### WIMIP - What Is My IP?
-
-A simple Python project to retrieve the public IP address of the user.
-
-##### Example:
-
+```text
+             _   _  __ _           
+ _ __   ___ | |_(_)/ _(_) ___ _ __ 
+| '_ \ / _ \| __| | |_| |/ _ \ '__|
+| | | | (_) | |_| |  _| |  __/ |   
+|_| |_|\___/ \__|_|_| |_|\___|_|   
 ```
-curl -s ip.tech1ndex.ca
-```
-1.1.1.1
+
+A simple Python project to fetch a list of free Games on the Epic Games Store and notify a group.
---
+++ src/notifier/main.py
@@ -0,0 +1,39 @@
+import logging
+import sys
+import time
+from logger.setup import setup_logger
+from bot.signal import SignalBot
+from external.epic import EpicFreeGames
+from settings import SignalBotSettings, EpicSettings
+from storage import SentGamesStorage
+
+def main():
+    setup_logger()
+    signal_settings = SignalBotSettings()
+    bot = SignalBot(signal_settings.signal_api_url, signal_settings.signal_phone)
+    storage = SentGamesStorage()
+    logging.info(f"Bot initialized with phone number {signal_settings.signal_phone}")
+
+    group_id = signal_settings.signal_group_id
+    epic_settings = EpicSettings()
+    epic = EpicFreeGames(epic_settings)
+
+    try:
+        while True:
+            games = epic.format_games_info()
+            for game in games:
+                if not storage.is_game_sent(game["store_url"]):
+                    message = f"New free game - {game['name']} - {game['store_url']}"
+                    if bot.send_group_message(group_id=group_id, message=message):
+                        storage.mark_game_sent(game["store_url"])
+
+            if signal_settings.one_time_run:
+                logging.info("One-time run completed. Exiting.")
+                sys.exit(0)
+
+            time.sleep(signal_settings.update_interval)
+    except KeyboardInterrupt:
+        logging.info("\nBot stopped by user")
+
+if __name__ == "__main__":
+    main()
